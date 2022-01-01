97fc1c2144018188aa4020e18f33d3640bbecbe6
Gautam Jain <astrogeek77@gmail.com>
2022-01-01 13:14:43 +0530

    Updated smart contracts and intialized rETH faucet account

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Krypt - Web 3.0 Blockchain Application
+# Crypto - Web 3.0 Blockchain Application
 ![Krypt](https://i.ibb.co/DVF4tNW/image.png)
 
 ## Introduction
-This is a code repository for the corresponding video tutorial.
+This is a code repository for the Web3.0 Blockchain Application Crypto
 
-Using Web 3.0 methodologies, Solidity and Metamask you'll learn how to build a your first real Web 3.0 Application - from start to finish.
-
+Built Using Web 3.0 methodologies(Hardhat etc), Solidity and Metamask.
--- smart_contracts/contracts/Transactions.sol
+++ smart_contracts/contracts/Transactions.sol
@@ -1,9 +1,9 @@
 //  SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0; //locking the working version
 
 contract Transactions {
     // type specification is mandatory
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     // An event defined for a praticular transaction
     event Transfer(
@@ -28,13 +28,47 @@ contract Transactions {
     // array for storing our transactions
     transferStruct[] transactions;
 
-    function addToBlockchain() public {}
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public payable{
+        // incrementing the counter
+        transactionCount++;
+
+        // Push our transaction details to the array
+        transactions.push(
+            transferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        // emitting tranfer to acctually perform the transaction
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
 
     function getAllTransactions()
         public
         view
         returns (transferStruct[] memory)
-    {}
+    {
+        return transactions;
+    }
 
-    function getTransactionsCount() public view returns (uint256) {}
+    function getTransactionsCount() public view returns (uint256) {
+        return transactionCount;
+    }
 }
